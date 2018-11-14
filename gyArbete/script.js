a2b6e7588223344523f2fdb3de5789d2a9186700
elaww <irsad.rencber@gmail.com>
2018-11-14 15:09:19 +0100

    Add files via upload

--- gyArbete/script.js
+++ gyArbete/script.js
@@ -14,4 +14,22 @@ function startCam() {
 }
 }
 
-//function reset() {}
+
+function reset() {
+  webcamStream.stop();
+
+}
+
+var canvas, ctx;
+
+      function init() {
+        // Get the canvas and obtain a context for
+        // drawing in it
+        canvas = document.getElementById("canvas");
+        ctx = canvas.getContext('2d');
+      }
+
+      function takeSnapshot() {
+         // Draws current image from the video element into the canvas
+        ctx.drawImage(video, 0,0, canvas.width, canvas.height);
+      }
